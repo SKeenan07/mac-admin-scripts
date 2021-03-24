49c38c4e027939a8e2303ac86cd492e8d95ea8b1
Sarah Keenan <48265613+SKeenan07@users.noreply.github.com>
2021-03-24 15:32:08 -0400

    Update autoLogout.zsh
    Replaced line to restart Mac with a command to log out the user. Also, added a function to quit all open applications.

--- auto-logout/autoLogout.zsh
+++ auto-logout/autoLogout.zsh
@@ -3,6 +3,29 @@
 # Sarah Keenan - January 13, 2021
 # This script automatically logs out (restarts) the Mac after a specified idle time. 
 
+quitRunningApplications () {
+	declare -a applications
+
+	allApplications=$(system_profiler SPApplicationsDataType | grep "Location:" | grep -v "/Library/" | sed 's|Location:||g' | sed 's|^[ \t]*||g' | awk -F "/" '{ print $NF }' | sed 's|.app||g')
+
+	# For every custom trigger, add it to the array
+	for i in "${(f)allApplications}"; do
+		applications+=( "$i" )
+	done
+
+	for i in "${applications[@]}"; do 
+		process=$(pgrep -x "$i")
+		if [[ -n "$process" ]]; then
+			n=0
+			while [[ -n "$process" ]] && [[ $n < 10 ]]; do
+				sudo killall "$i" && sleep 1
+				process=$(pgrep -x "$i")
+				let n++
+			done
+		fi
+	done
+}
+
 # Are users logged in?
 loggedInUsers=$(last | grep "still logged in" | grep "console" | awk '{print $1}')
 
@@ -29,7 +52,9 @@ if [[ -n "$loggedInUsers" ]]; then
 		answer=$("$JAMF_HELPER" -windowType "$window" -title "$title" -heading "$heading" -description "$message" -icon "$icon" -button1 "$buttonone" -button2 "$buttontwo" -timeout "$timeout" -countdown)
 
 		if [[ $answer == 0 ]]; then
-			sudo shutdown -r now
+			quitRunningApplications			
+			currentUser=$(stat -f%Su /dev/console)
+			sudo launchctl bootout user/$(id -u $currentUser)
 		fi
 	fi
 fi
