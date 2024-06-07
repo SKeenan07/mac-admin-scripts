9955b192acf94ad9b5b72ac29f7b1d66fb93cc9f
Sarah Keenan <48265613+SKeenan07@users.noreply.github.com>
2024-06-07 11:27:54 -0400

    Update autoLogout.zsh
    Fix issue where the script would log out the currently active user if there was another inactive user logged in

--- auto-logout/autoLogout.zsh
+++ auto-logout/autoLogout.zsh
@@ -3,6 +3,24 @@
 # Sarah Keenan - January 13, 2021
 # This script automatically logs out the Mac after a specified idle time. 
 
+# Update June 7, 2024
+
+##### Functions
+
+prompt () {
+	JAMF_HELPER="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
+	window="utility"
+	title="OHIO Idle Logout"
+	heading="This Mac is idle."
+	message='This Mac has been idle for more than 15 minutes. Click the "More Time" button to continue using this Mac. Otherwise, an automatic logout will occur and all unsaved documents will be LOST!'
+	icon="/Library/Application Support/JAMF/bin/brandingimage.png"
+	buttonone="Log Out" # $answer will be 0 if selected
+	buttontwo="More Time" # $answer will be 2 if selected
+	timeout="300" # $answer will be 0 if the popup times out
+
+	"$JAMF_HELPER" -windowType "$window" -title "$title" -heading "$heading" -description "$message" -icon "$icon" -button1 "$buttonone" -button2 "$buttontwo" -timeout "$timeout" -countdown
+}
+
 quitRunningApplications () {
 	declare -a applications
 
@@ -26,35 +44,56 @@ quitRunningApplications () {
 	done
 }
 
+getIdleTime () {
+	ioreg -c IOHIDSystem | awk '/HIDIdleTime/ { print $NF/1000000000 }' | sed 's|\.| |g' | awk '{ print $1 }'
+}
+
+logoutUser () {
+	sudo launchctl bootout user/$(id -u ${1})
+}
+
+##### Variables
+
+maxIdleTime=900 # 15 minutes
+
+currentUser=$(stat -f%Su /dev/console)
+echo "Current User: $currentUser"
+
 # Are users logged in?
-loggedInUsers=$(last | grep "still logged in" | grep "console" | awk '{print $1}')
+# loggedInUsers=$(last | grep "still logged in" | grep "console" | awk '{print $1}')
+loggedInUsers=$(last | grep "still logged in" | awk '{print $1}' | sort -u)
+echo "Logged in Users: $loggedInUsers"
 
 # If there are users logged in, get the idle time
 if [[ -n "$loggedInUsers" ]]; then
 
-	idleTime=$(ioreg -c IOHIDSystem | awk '/HIDIdleTime/ { print $NF/1000000000 }' | sed 's|\.| |g' | awk '{ print $1 }')
-	maxIdleTime=970
+	for user in ${(f)loggedInUsers[@]}; do
 
-	if (( $idleTime > $maxIdleTime )); then
-	
-		# Configure Log out Popup
-		JAMF_HELPER="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
-		window="utility"
-		title="Your Organization Idle Logout"
-		heading="This Mac is idle."
-		message='This Mac has been idle for more than 15 minutes. Click the "More Time" button to continue using this Mac. Otherwise, an automatic logout will occur and all unsaved documents will be LOST!'
-		icon="/Library/Application Support/JAMF/bin/brandingimage.png"
-		buttonone="Log Out" # $answer will be 0 if selected
-		buttontwo="More Time" # $answer will be 2 if selected
-		timeout="300" # $answer will be 0 if the popup times out
-		
-		# Prompt the user
-		answer=$("$JAMF_HELPER" -windowType "$window" -title "$title" -heading "$heading" -description "$message" -icon "$icon" -button1 "$buttonone" -button2 "$buttontwo" -timeout "$timeout" -countdown)
+		# If the user is not the current user, log them out
+		if [[ "$user" != "$currentUser" ]]; then
+			echo "$user is not the current user."
+			echo "Logging out $user..."
+			logoutUser $user
 
-		if [[ $answer == 0 ]]; then
-			quitRunningApplications			
-			currentUser=$(stat -f%Su /dev/console)
-			sudo launchctl bootout user/$(id -u $currentUser)
+		# If the user is the current user, get the idle time
+		else
+			idleTime=$(getIdleTime)
+			echo "Idle Time for $currentUser: $idleTime"
+			
+			# If the idle time is greater than the max idle time, prompt the user
+			if (( $idleTime > $maxIdleTime )); then
+				echo "Idle Time is greater than $maxIdleTime. Prompting user..."
+				answer=$(prompt)
+
+				if [[ $answer == 0 ]]; then
+					echo "Logging out $currentUser..."
+					quitRunningApplications
+					logoutUser $user
+				fi
+			fi
+
 		fi
-	fi
+
+	done
+
 fi
